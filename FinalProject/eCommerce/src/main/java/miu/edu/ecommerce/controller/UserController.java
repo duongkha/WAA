454f19d3801dc64fa02fd1faf67ad73daa5191b9
Kha Nguyen <dknguyen@miu.edu>
2021-05-09 23:24:16 -0500

    get current user info

--- FinalProject/eCommerce/src/main/java/miu/edu/ecommerce/controller/UserController.java
+++ FinalProject/eCommerce/src/main/java/miu/edu/ecommerce/controller/UserController.java
@@ -1,14 +1,26 @@
 package miu.edu.ecommerce.controller;
 
+import miu.edu.ecommerce.service.UserDetailsImpl;
+import miu.edu.ecommerce.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping({ "/getuserinfo" })
-    public String getUserInfo() {
-        return "User Info";
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @GetMapping({ "/current" })
+    public UserDetailsImpl getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return (UserDetailsImpl)auth.getPrincipal();
     }
 
 }
