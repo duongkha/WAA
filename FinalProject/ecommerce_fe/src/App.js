423cfbc96ff58aea67dc4d3d457402631324973d
Kha Nguyen <dknguyen@miu.edu>
2021-05-11 14:33:40 -0500

    fix app.js

--- FinalProject/ecommerce_fe/src/App.js
+++ FinalProject/ecommerce_fe/src/App.js
@@ -1,59 +1,231 @@
 import logo from './logo.svg';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link,Redirect } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link, Redirect, BrowserRouter} from "react-router-dom";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
-import {APIConfig} from "./store/API-Config";
-import React from "react";
+//import {APIConfig} from "./store/API-Config";
+import { signout } from './actions/userActions';
 import Home from "./containers/Home/Home";
 
 
-function App() {
-  return (
-      <APIConfig.Provider value={
-        {
-          registerAPI: 'http://localhost:8080/register',
-          loginAPI: 'http://localhost:8080/authenticate',
-          logoutAPI: 'http://localhost:8080/logout',
-          productAPI: 'http://localhost:8080/products',
-          orderAPI: 'http://localhost:8080/orders',
-          sellerAPI: 'http://localhost:8080/sellers',
-          categoryAPI: 'http://localhost:8080/categories',
-          roleAPI: 'http://localhost:8080/roles',
-          userAPI: 'http://localhost:8080/user',
-        }
-      }>
-        <Router>
-          <div className="App">
-            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-              <div className="container">
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                  <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                      <Link className="nav-link" to={"/sign-in"}>Login</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
 
-            <div className="auth-wrapper">
-              <div className="auth-inner">
-                <Switch>
-                  <Route exact path='/' component={Home} />
-                  <Route path="/sign-in" component={Login} />
-                  <Route path="/sign-up" component={SignUp} />
-                  <Redirect from="/" to="/home" />
-                </Switch>
-              </div>
+function App() {
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  // const userSignin = useSelector((state) => state.userSignin);
+  // const { userInfo } = userSignin;
+
+  // const dispatch = useDispatch();
+  // const signoutHandler = () => {
+  //   dispatch(signout());
+  // };
+
+  // const productCategoryList = useSelector((state) => state.productCategoryList);
+  // const {
+  //   loading: loadingCategories,
+  //   error: errorCategories,
+  //   categories,
+  // } = productCategoryList;
+
+  return (
+      <BrowserRouter>
+        <div className="grid-container">
+          <header className="row">
+            <div>
+              <button
+                  type="button"
+                  className="open-sidebar"
+                  onClick={() => setSidebarIsOpen(true)}
+              >
+                <i className="fa fa-bars"></i>
+              </button>
+              <Link className="brand" to="/">
+                WAA
+              </Link>
             </div>
-          </div>
-        </Router>
-      </APIConfig.Provider>
+
+            <div>
+              <Link to="/cart">
+                Cart
+                {/*{cartItems.length > 0 && (*/}
+                {/*    <span className="badge">{cartItems.length}</span>*/}
+                {/*)}*/}
+              </Link>
+
+              {/*{userInfo ? (*/}
+              {/*    <div className="dropdown">*/}
+              {/*      <Link to="#">*/}
+              {/*        {userInfo.name} <i className="fa fa-caret-down"></i>{' '}*/}
+              {/*      </Link>*/}
+              {/*      <ul className="dropdown-content">*/}
+              {/*        <li>*/}
+              {/*          <Link to="/profile">User Profile</Link>*/}
+              {/*        </li>*/}
+              {/*        <li>*/}
+              {/*          <Link to="/orderhistory">Order History</Link>*/}
+              {/*        </li>*/}
+              {/*        <li>*/}
+              {/*          <Link to="#signout" onClick={signoutHandler}>*/}
+              {/*            Sign Out*/}
+              {/*          </Link>*/}
+              {/*        </li>*/}
+              {/*      </ul>*/}
+              {/*    </div>*/}
+              {/*) : (*/}
+              {/*    <Link to="/signin">Sign In</Link>*/}
+              {/*)}*/}
+              {/*{userInfo && userInfo.isSeller && (*/}
+              {/*    <div className="dropdown">*/}
+              {/*      <Link to="#admin">*/}
+              {/*        Seller <i className="fa fa-caret-down"></i>*/}
+              {/*      </Link>*/}
+              {/*      <ul className="dropdown-content">*/}
+              {/*        <li>*/}
+              {/*          <Link to="/productlist/seller">Products</Link>*/}
+              {/*        </li>*/}
+              {/*        <li>*/}
+              {/*          <Link to="/orderlist/seller">Orders</Link>*/}
+              {/*        </li>*/}
+              {/*      </ul>*/}
+              {/*    </div>*/}
+              {/*)}*/}
+              {/*{userInfo && userInfo.isAdmin && (*/}
+              {/*    <div className="dropdown">*/}
+              {/*      <Link to="#admin">*/}
+              {/*        Admin <i className="fa fa-caret-down"></i>*/}
+              {/*      </Link>*/}
+              {/*      <ul className="dropdown-content">*/}
+              {/*        <li>*/}
+              {/*          <Link to="/dashboard">Dashboard</Link>*/}
+              {/*        </li>*/}
+              {/*        <li>*/}
+              {/*          <Link to="/productlist">Products</Link>*/}
+              {/*        </li>*/}
+              {/*        <li>*/}
+              {/*          <Link to="/orderlist">Orders</Link>*/}
+              {/*        </li>*/}
+              {/*        <li>*/}
+              {/*          <Link to="/userlist">Users</Link>*/}
+              {/*        </li>*/}
+              {/*        <li>*/}
+              {/*          <Link to="/support">Support</Link>*/}
+              {/*        </li>*/}
+              {/*      </ul>*/}
+              {/*    </div>*/}
+              {/*)}*/}
+            </div>
+          </header>
+          <aside className={sidebarIsOpen ? 'open' : ''}>
+            <ul className="categories">
+              <li>
+                <strong>Categories</strong>
+                <button
+                    onClick={() => setSidebarIsOpen(false)}
+                    className="close-sidebar"
+                    type="button"
+                >
+                  <i className="fa fa-close"></i>
+                </button>
+              </li>
+              {/*{(*/}
+              {/*    categories.map((c) => (*/}
+              {/*        <li key={c}>*/}
+              {/*          <Link*/}
+              {/*              to={`/search/category/${c}`}*/}
+              {/*              onClick={() => setSidebarIsOpen(false)}*/}
+              {/*          >*/}
+              {/*            {c}*/}
+              {/*          </Link>*/}
+              {/*        </li>*/}
+              {/*    ))*/}
+              {/*)}*/}
+            </ul>
+          </aside>
+          <main>
+            {/*<Route path="/seller/:id" component={SellerScreen}></Route>*/}
+            {/*<Route path="/cart/:id?" component={CartScreen}></Route>*/}
+            {/*<Route path="/product/:id" component={ProductScreen} exact></Route>*/}
+            {/*<Route*/}
+            {/*    path="/product/:id/edit"*/}
+            {/*    component={ProductEditScreen}*/}
+            {/*    exact*/}
+            {/*></Route>*/}
+            {/*<Route path="/login" component={Login}></Route>*/}
+            <Route path="/register" component={SignUp}></Route>
+            {/*<Route path="/shipping" component={ShippingAddressScreen}></Route>*/}
+            {/*<Route path="/payment" component={PaymentMethodScreen}></Route>*/}
+            {/*<Route path="/placeorder" component={PlaceOrderScreen}></Route>*/}
+            {/*<Route path="/order/:id" component={OrderScreen}></Route>*/}
+            {/*<Route path="/orderhistory" component={OrderHistoryScreen}></Route>*/}
+            {/*<Route*/}
+            {/*    path="/search/name/:name?"*/}
+            {/*    component={SearchScreen}*/}
+            {/*    exact*/}
+            {/*></Route>*/}
+            {/*<Route*/}
+            {/*    path="/search/category/:category"*/}
+            {/*    component={SearchScreen}*/}
+            {/*    exact*/}
+            {/*></Route>*/}
+            {/*<Route*/}
+            {/*    path="/search/category/:category/name/:name"*/}
+            {/*    component={SearchScreen}*/}
+            {/*    exact*/}
+            {/*></Route>*/}
+            {/*<Route*/}
+            {/*    path="/search/category/:category/name/:name/min/:min/max/:max/rating/:rating/order/:order/pageNumber/:pageNumber"*/}
+            {/*    component={SearchScreen}*/}
+            {/*    exact*/}
+            {/*></Route>*/}
+            {/*<PrivateRoute*/}
+            {/*    path="/profile"*/}
+            {/*    component={ProfileScreen}*/}
+            {/*></PrivateRoute>*/}
+            {/*<PrivateRoute path="/map" component={MapScreen}></PrivateRoute>*/}
+            {/*<AdminRoute*/}
+            {/*    path="/productlist"*/}
+            {/*    component={ProductListScreen}*/}
+            {/*    exact*/}
+            {/*></AdminRoute>*/}
+            {/*<AdminRoute*/}
+            {/*    path="/productlist/pageNumber/:pageNumber"*/}
+            {/*    component={ProductListScreen}*/}
+            {/*    exact*/}
+            {/*></AdminRoute>*/}
+            {/*<AdminRoute*/}
+            {/*    path="/orderlist"*/}
+            {/*    component={OrderListScreen}*/}
+            {/*    exact*/}
+            {/*></AdminRoute>*/}
+            {/*<AdminRoute path="/userlist" component={UserListScreen}></AdminRoute>*/}
+            {/*<AdminRoute*/}
+            {/*    path="/user/:id/edit"*/}
+            {/*    component={UserEditScreen}*/}
+            {/*></AdminRoute>*/}
+
+            {/*<AdminRoute*/}
+            {/*    path="/dashboard"*/}
+            {/*    component={DashboardScreen}*/}
+            {/*></AdminRoute>*/}
+            {/*<AdminRoute path="/support" component={SupportScreen}></AdminRoute>*/}
+
+            {/*<SellerRoute*/}
+            {/*    path="/productlist/seller"*/}
+            {/*    component={ProductListScreen}*/}
+            {/*></SellerRoute>*/}
+            {/*<SellerRoute*/}
+            {/*    path="/orderlist/seller"*/}
+            {/*    component={OrderListScreen}*/}
+            {/*></SellerRoute>*/}
+
+            <Route path="/" component={Home} exact></Route>
+          </main>
+          <footer className="row center">
+            <div>All right reserved</div>{' '}
+          </footer>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- FinalProject/ecommerce_fe/src/store/store.js
+++ FinalProject/ecommerce_fe/src/store/store.js
@@ -1,30 +1,34 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { cartReducer } from './reducers/cartReducers';
-
-
+import {
+  userSigninReducer,
+} from './reducers/userReducers';
 
 const initialState = {
-
-  cart: {
-    cartItems: localStorage.getItem('cartItems')
-      ? JSON.parse(localStorage.getItem('cartItems'))
-      : [],
-    shippingAddress: localStorage.getItem('shippingAddress')
-      ? JSON.parse(localStorage.getItem('shippingAddress'))
-      : {},
-    paymentMethod: 'PayPal',
-  },
+  // userSignin: {
+  //   userInfo: localStorage.getItem('userInfo')
+  //       ? JSON.parse(localStorage.getItem('userInfo'))
+  //       : null,
+  // },
+  // cart: {
+  //   cartItems: localStorage.getItem('cartItems')
+  //       ? JSON.parse(localStorage.getItem('cartItems'))
+  //       : [],
+  //   shippingAddress: localStorage.getItem('shippingAddress')
+  //       ? JSON.parse(localStorage.getItem('shippingAddress'))
+  //       : {},
+  //   paymentMethod: 'PayPal',
+  // },
 };
 const reducer = combineReducers({
-  cart: cartReducer,
-
+  //userSignin: userSigninReducer,
 });
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducer,
-  initialState,
-  composeEnhancer(applyMiddleware(thunk))
+    reducer//,
+    //initialState,
+    //composeEnhancer(applyMiddleware(thunk))
 );
 
 export default store;
