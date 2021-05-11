b0a3e7a920ffaa06a9ef332351530ef1c29d6984
pascalbenimana6 <pascal.benimana6@gmail.com>
2021-05-11 10:37:23 -0500

    local only
    commit to local

--- FinalProject/frontend/src/index.js
+++ FinalProject/frontend/src/index.js
@@ -1,17 +1,21 @@
+//Main React Entry File
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { store } from './_helpers';
+import { App } from './App';
+/*The boilerplate application uses a fake / mock backend that stores data 
+in browser local storage, to switch to a real backend api simply remove the 
+fake backend code below the comment
+*/
+// setup fake backend
+import { configureFakeBackend } from './_helpers';
+configureFakeBackend();
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
