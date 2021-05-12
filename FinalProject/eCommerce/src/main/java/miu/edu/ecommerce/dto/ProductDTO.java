3c80a61f29d49c9711f9c841d8ac121e20859afa
SaNguyen <sa.nguyentl@gmail.com>
2021-05-12 11:22:10 -0500

    Updated name column

--- FinalProject/eCommerce/src/main/java/miu/edu/ecommerce/dto/ProductDTO.java
+++ FinalProject/eCommerce/src/main/java/miu/edu/ecommerce/dto/ProductDTO.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import miu.edu.ecommerce.domain.Category;
 
+import javax.persistence.Column;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,14 +17,15 @@ public class ProductDTO {
     private String productName;
 
     private String producer;
-
-    private String desciption;
+    private String description;
     private String color;
     private String size;
     private Double price;
+    private Double rating;
+    private Integer numReviews;
     private LocalDate dueDate;
     private int quantityInStock;
-//    private CategoryDTO category;
+    private CategoryDTO category;
 //    private List<ReviewDTO> reviews = new ArrayList();
 
 }
