811bec52ba83e16c825ded8d49aa3bd69632ed22
qianzhisanyao <75343488+qianzhisanyao@users.noreply.github.com>
2021-05-12 10:12:29 -0500

    update home

--- FinalProject/ecommerce_fe/src/App.js
+++ FinalProject/ecommerce_fe/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link, Redirect, BrowserRouter} from "react-router-dom";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
-//import {APIConfig} from "./store/API-Config";
+import {APIConfig} from "./store/API-Config";
 import Home from "./containers/Home/Home";
 
 
@@ -28,203 +28,217 @@ function App() {
   // } = productCategoryList;
 
   return (
-      <BrowserRouter>
-        <div className="grid-container">
-          <header className="row">
-            <div>
-              <button
-                  type="button"
-                  className="open-sidebar"
-                  onClick={() => setSidebarIsOpen(true)}
-              >
-                <i className="fa fa-bars"></i>
-              </button>
-              <Link className="brand" to="/">
-                WAA
-              </Link>
-            </div>
-
-            <div>
-              <Link to="/cart">
-                Cart
-                {/*{cartItems.length > 0 && (*/}
-                {/*    <span className="badge">{cartItems.length}</span>*/}
-                {/*)}*/}
-              </Link>
-
-              {/*{userInfo ? (*/}
-              {/*    <div className="dropdown">*/}
-              {/*      <Link to="#">*/}
-              {/*        {userInfo.name} <i className="fa fa-caret-down"></i>{' '}*/}
-              {/*      </Link>*/}
-              {/*      <ul className="dropdown-content">*/}
-              {/*        <li>*/}
-              {/*          <Link to="/profile">User Profile</Link>*/}
-              {/*        </li>*/}
-              {/*        <li>*/}
-              {/*          <Link to="/orderhistory">Order History</Link>*/}
-              {/*        </li>*/}
-              {/*        <li>*/}
-              {/*          <Link to="#signout" onClick={signoutHandler}>*/}
-              {/*            Sign Out*/}
-              {/*          </Link>*/}
-              {/*        </li>*/}
-              {/*      </ul>*/}
-              {/*    </div>*/}
-              {/*) : (*/}
-              {/*    <Link to="/signin">Sign In</Link>*/}
-              {/*)}*/}
-              {/*{userInfo && userInfo.isSeller && (*/}
-              {/*    <div className="dropdown">*/}
-              {/*      <Link to="#admin">*/}
-              {/*        Seller <i className="fa fa-caret-down"></i>*/}
-              {/*      </Link>*/}
-              {/*      <ul className="dropdown-content">*/}
-              {/*        <li>*/}
-              {/*          <Link to="/productlist/seller">Products</Link>*/}
-              {/*        </li>*/}
-              {/*        <li>*/}
-              {/*          <Link to="/orderlist/seller">Orders</Link>*/}
-              {/*        </li>*/}
-              {/*      </ul>*/}
-              {/*    </div>*/}
-              {/*)}*/}
-              {/*{userInfo && userInfo.isAdmin && (*/}
-              {/*    <div className="dropdown">*/}
-              {/*      <Link to="#admin">*/}
-              {/*        Admin <i className="fa fa-caret-down"></i>*/}
-              {/*      </Link>*/}
-              {/*      <ul className="dropdown-content">*/}
-              {/*        <li>*/}
-              {/*          <Link to="/dashboard">Dashboard</Link>*/}
-              {/*        </li>*/}
-              {/*        <li>*/}
-              {/*          <Link to="/productlist">Products</Link>*/}
-              {/*        </li>*/}
-              {/*        <li>*/}
-              {/*          <Link to="/orderlist">Orders</Link>*/}
-              {/*        </li>*/}
-              {/*        <li>*/}
-              {/*          <Link to="/userlist">Users</Link>*/}
-              {/*        </li>*/}
-              {/*        <li>*/}
-              {/*          <Link to="/support">Support</Link>*/}
-              {/*        </li>*/}
-              {/*      </ul>*/}
-              {/*    </div>*/}
-              {/*)}*/}
-            </div>
-          </header>
-          <aside className={sidebarIsOpen ? 'open' : ''}>
-            <ul className="categories">
-              <li>
-                <strong>Categories</strong>
+    <APIConfig.Provider value={
+      {
+          registerAPI: 'http://localhost:8080/register',
+          loginAPI: 'http://localhost:8080/authenticate',
+          logoutAPI: 'http://localhost:8080/logout',
+          productAPI: 'http://localhost:8080/products',
+          orderAPI: 'http://localhost:8080/orders',
+          sellerAPI: 'http://localhost:8080/sellers',
+          categoryAPI: 'http://localhost:8080/categories',
+          roleAPI: 'http://localhost:8080/roles',
+          userAPI: 'http://localhost:8080/user',
+      }
+  }>
+        <BrowserRouter>
+          <div className="grid-container">
+            <header className="row">
+              <div>
                 <button
-                    onClick={() => setSidebarIsOpen(false)}
-                    className="close-sidebar"
                     type="button"
+                    className="open-sidebar"
+                    onClick={() => setSidebarIsOpen(true)}
                 >
-                  <i className="fa fa-close"></i>
+                  <i className="fa fa-bars"></i>
                 </button>
-              </li>
-              {/*{(*/}
-              {/*    categories.map((c) => (*/}
-              {/*        <li key={c}>*/}
-              {/*          <Link*/}
-              {/*              to={`/search/category/${c}`}*/}
-              {/*              onClick={() => setSidebarIsOpen(false)}*/}
-              {/*          >*/}
-              {/*            {c}*/}
-              {/*          </Link>*/}
-              {/*        </li>*/}
-              {/*    ))*/}
-              {/*)}*/}
-            </ul>
-          </aside>
-          <main>
-            {/*<Route path="/seller/:id" component={SellerScreen}></Route>*/}
-            {/*<Route path="/cart/:id?" component={CartScreen}></Route>*/}
-            {/*<Route path="/product/:id" component={ProductScreen} exact></Route>*/}
-            {/*<Route*/}
-            {/*    path="/product/:id/edit"*/}
-            {/*    component={ProductEditScreen}*/}
-            {/*    exact*/}
-            {/*></Route>*/}
-            {/*<Route path="/login" component={Login}></Route>*/}
-            <Route path="/register" component={SignUp}></Route>
-            {/*<Route path="/shipping" component={ShippingAddressScreen}></Route>*/}
-            {/*<Route path="/payment" component={PaymentMethodScreen}></Route>*/}
-            {/*<Route path="/placeorder" component={PlaceOrderScreen}></Route>*/}
-            {/*<Route path="/order/:id" component={OrderScreen}></Route>*/}
-            {/*<Route path="/orderhistory" component={OrderHistoryScreen}></Route>*/}
-            {/*<Route*/}
-            {/*    path="/search/name/:name?"*/}
-            {/*    component={SearchScreen}*/}
-            {/*    exact*/}
-            {/*></Route>*/}
-            {/*<Route*/}
-            {/*    path="/search/category/:category"*/}
-            {/*    component={SearchScreen}*/}
-            {/*    exact*/}
-            {/*></Route>*/}
-            {/*<Route*/}
-            {/*    path="/search/category/:category/name/:name"*/}
-            {/*    component={SearchScreen}*/}
-            {/*    exact*/}
-            {/*></Route>*/}
-            {/*<Route*/}
-            {/*    path="/search/category/:category/name/:name/min/:min/max/:max/rating/:rating/order/:order/pageNumber/:pageNumber"*/}
-            {/*    component={SearchScreen}*/}
-            {/*    exact*/}
-            {/*></Route>*/}
-            {/*<PrivateRoute*/}
-            {/*    path="/profile"*/}
-            {/*    component={ProfileScreen}*/}
-            {/*></PrivateRoute>*/}
-            {/*<PrivateRoute path="/map" component={MapScreen}></PrivateRoute>*/}
-            {/*<AdminRoute*/}
-            {/*    path="/productlist"*/}
-            {/*    component={ProductListScreen}*/}
-            {/*    exact*/}
-            {/*></AdminRoute>*/}
-            {/*<AdminRoute*/}
-            {/*    path="/productlist/pageNumber/:pageNumber"*/}
-            {/*    component={ProductListScreen}*/}
-            {/*    exact*/}
-            {/*></AdminRoute>*/}
-            {/*<AdminRoute*/}
-            {/*    path="/orderlist"*/}
-            {/*    component={OrderListScreen}*/}
-            {/*    exact*/}
-            {/*></AdminRoute>*/}
-            {/*<AdminRoute path="/userlist" component={UserListScreen}></AdminRoute>*/}
-            {/*<AdminRoute*/}
-            {/*    path="/user/:id/edit"*/}
-            {/*    component={UserEditScreen}*/}
-            {/*></AdminRoute>*/}
+                <Link className="brand" to="/">
+                  Shopping
+                </Link>
+              </div>
 
-            {/*<AdminRoute*/}
-            {/*    path="/dashboard"*/}
-            {/*    component={DashboardScreen}*/}
-            {/*></AdminRoute>*/}
-            {/*<AdminRoute path="/support" component={SupportScreen}></AdminRoute>*/}
+              <div>
+                <Link to="/cart">
+                  Cart
+                  {/*{cartItems.length > 0 && (*/}
+                  {/*    <span className="badge">{cartItems.length}</span>*/}
+                  {/*)}*/}
+                </Link>
 
-            {/*<SellerRoute*/}
-            {/*    path="/productlist/seller"*/}
-            {/*    component={ProductListScreen}*/}
-            {/*></SellerRoute>*/}
-            {/*<SellerRoute*/}
-            {/*    path="/orderlist/seller"*/}
-            {/*    component={OrderListScreen}*/}
-            {/*></SellerRoute>*/}
+                {/*{userInfo ? (*/}
+                {/*    <div className="dropdown">*/}
+                {/*      <Link to="#">*/}
+                {/*        {userInfo.name} <i className="fa fa-caret-down"></i>{' '}*/}
+                {/*      </Link>*/}
+                {/*      <ul className="dropdown-content">*/}
+                {/*        <li>*/}
+                {/*          <Link to="/profile">User Profile</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*          <Link to="/orderhistory">Order History</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*          <Link to="#signout" onClick={signoutHandler}>*/}
+                {/*            Sign Out*/}
+                {/*          </Link>*/}
+                {/*        </li>*/}
+                {/*      </ul>*/}
+                {/*    </div>*/}
+                {/*) : (*/}
+                {/*    <Link to="/signin">Sign In</Link>*/}
+                {/*)}*/}
+                {/*{userInfo && userInfo.isSeller && (*/}
+                {/*    <div className="dropdown">*/}
+                {/*      <Link to="#admin">*/}
+                {/*        Seller <i className="fa fa-caret-down"></i>*/}
+                {/*      </Link>*/}
+                {/*      <ul className="dropdown-content">*/}
+                {/*        <li>*/}
+                {/*          <Link to="/productlist/seller">Products</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*          <Link to="/orderlist/seller">Orders</Link>*/}
+                {/*        </li>*/}
+                {/*      </ul>*/}
+                {/*    </div>*/}
+                {/*)}*/}
+                {/*{userInfo && userInfo.isAdmin && (*/}
+                {/*    <div className="dropdown">*/}
+                {/*      <Link to="#admin">*/}
+                {/*        Admin <i className="fa fa-caret-down"></i>*/}
+                {/*      </Link>*/}
+                {/*      <ul className="dropdown-content">*/}
+                {/*        <li>*/}
+                {/*          <Link to="/dashboard">Dashboard</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*          <Link to="/productlist">Products</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*          <Link to="/orderlist">Orders</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*          <Link to="/userlist">Users</Link>*/}
+                {/*        </li>*/}
+                {/*        <li>*/}
+                {/*          <Link to="/support">Support</Link>*/}
+                {/*        </li>*/}
+                {/*      </ul>*/}
+                {/*    </div>*/}
+                {/*)}*/}
+              </div>
+            </header>
+            <aside className={sidebarIsOpen ? 'open' : ''}>
+              <ul className="categories">
+                <li>
+                  <strong>Categories</strong>
+                  <button
+                      onClick={() => setSidebarIsOpen(false)}
+                      className="close-sidebar"
+                      type="button"
+                  >
+                    <i className="fa fa-close"></i>
+                  </button>
+                </li>
+                {/*{(*/}
+                {/*    categories.map((c) => (*/}
+                {/*        <li key={c}>*/}
+                {/*          <Link*/}
+                {/*              to={`/search/category/${c}`}*/}
+                {/*              onClick={() => setSidebarIsOpen(false)}*/}
+                {/*          >*/}
+                {/*            {c}*/}
+                {/*          </Link>*/}
+                {/*        </li>*/}
+                {/*    ))*/}
+                {/*)}*/}
+              </ul>
+            </aside>
+            <main>
+              {/*<Route path="/seller/:id" component={SellerScreen}></Route>*/}
+              {/*<Route path="/cart/:id?" component={CartScreen}></Route>*/}
+              {/*<Route path="/product/:id" component={ProductScreen} exact></Route>*/}
+              {/*<Route*/}
+              {/*    path="/product/:id/edit"*/}
+              {/*    component={ProductEditScreen}*/}
+              {/*    exact*/}
+              {/*></Route>*/}
+              {/*<Route path="/login" component={Login}></Route>*/}
+              <Route path="/register" component={SignUp}></Route>
+              {/*<Route path="/shipping" component={ShippingAddressScreen}></Route>*/}
+              {/*<Route path="/payment" component={PaymentMethodScreen}></Route>*/}
+              {/*<Route path="/placeorder" component={PlaceOrderScreen}></Route>*/}
+              {/*<Route path="/order/:id" component={OrderScreen}></Route>*/}
+              {/*<Route path="/orderhistory" component={OrderHistoryScreen}></Route>*/}
+              {/*<Route*/}
+              {/*    path="/search/name/:name?"*/}
+              {/*    component={SearchScreen}*/}
+              {/*    exact*/}
+              {/*></Route>*/}
+              {/*<Route*/}
+              {/*    path="/search/category/:category"*/}
+              {/*    component={SearchScreen}*/}
+              {/*    exact*/}
+              {/*></Route>*/}
+              {/*<Route*/}
+              {/*    path="/search/category/:category/name/:name"*/}
+              {/*    component={SearchScreen}*/}
+              {/*    exact*/}
+              {/*></Route>*/}
+              {/*<Route*/}
+              {/*    path="/search/category/:category/name/:name/min/:min/max/:max/rating/:rating/order/:order/pageNumber/:pageNumber"*/}
+              {/*    component={SearchScreen}*/}
+              {/*    exact*/}
+              {/*></Route>*/}
+              {/*<PrivateRoute*/}
+              {/*    path="/profile"*/}
+              {/*    component={ProfileScreen}*/}
+              {/*></PrivateRoute>*/}
+              {/*<PrivateRoute path="/map" component={MapScreen}></PrivateRoute>*/}
+              {/*<AdminRoute*/}
+              {/*    path="/productlist"*/}
+              {/*    component={ProductListScreen}*/}
+              {/*    exact*/}
+              {/*></AdminRoute>*/}
+              {/*<AdminRoute*/}
+              {/*    path="/productlist/pageNumber/:pageNumber"*/}
+              {/*    component={ProductListScreen}*/}
+              {/*    exact*/}
+              {/*></AdminRoute>*/}
+              {/*<AdminRoute*/}
+              {/*    path="/orderlist"*/}
+              {/*    component={OrderListScreen}*/}
+              {/*    exact*/}
+              {/*></AdminRoute>*/}
+              {/*<AdminRoute path="/userlist" component={UserListScreen}></AdminRoute>*/}
+              {/*<AdminRoute*/}
+              {/*    path="/user/:id/edit"*/}
+              {/*    component={UserEditScreen}*/}
+              {/*></AdminRoute>*/}
 
-            <Route path="/" component={Home} exact></Route>
-          </main>
-          <footer className="row center">
-            <div>All right reserved</div>{' '}
-          </footer>
-        </div>
-      </BrowserRouter>
+              {/*<AdminRoute*/}
+              {/*    path="/dashboard"*/}
+              {/*    component={DashboardScreen}*/}
+              {/*></AdminRoute>*/}
+              {/*<AdminRoute path="/support" component={SupportScreen}></AdminRoute>*/}
+
+              {/*<SellerRoute*/}
+              {/*    path="/productlist/seller"*/}
+              {/*    component={ProductListScreen}*/}
+              {/*></SellerRoute>*/}
+              {/*<SellerRoute*/}
+              {/*    path="/orderlist/seller"*/}
+              {/*    component={OrderListScreen}*/}
+              {/*></SellerRoute>*/}
+
+              <Route path="/" component={Home} exact></Route>
+            </main>
+            <footer className="row center">
+              <div>Welcome</div>{' '}
+            </footer>
+          </div>
+        </BrowserRouter>
+      </APIConfig.Provider>
   );
 }
 
--- FinalProject/ecommerce_fe/src/components/Product/Product.js
+++ FinalProject/ecommerce_fe/src/components/Product/Product.js
@@ -6,18 +6,18 @@ export default function Product(props) {
   const { product } = props;
   return (
     <div key={product._id} className="card">
-      <Link to={`/product/${product._id}`}>
-        <img className="medium" src={product.image} alt={product.name} />
+      <Link to={`/product/${product.id}`}>
+        <img className="medium" src={product.photo} alt={product.productName} />
       </Link>
       <div className="card-body">
-        <Link to={`/product/${product._id}`}>
+        <Link to={`/product/${product.id}`}>
           <h2>{product.name}</h2>
         </Link>
         <div className="row">
           <div className="price">${product.price}</div>
           <div>
-            <Link to={`/seller/${product.seller._id}`}>
-              {product.seller.seller.name}
+            <Link to={`/seller/${product.producter}`}>
+              {product.producter}
             </Link>
           </div>
         </div>
