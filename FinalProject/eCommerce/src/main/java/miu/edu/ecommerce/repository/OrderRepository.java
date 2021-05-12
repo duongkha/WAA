2f4327be9b3113eac56e4b922c0b437ed8c8801d
Viveksan <viveksan7@gmail.com>
2021-05-11 23:37:07 -0500

    Revert "Added Order - Controller, DTO, Service and repository"
    This reverts commit c62cbc7455ffda3761042a32f8302569ccfcaccd.

--- FinalProject/eCommerce/src/main/java/miu/edu/ecommerce/repository/OrderRepository.java
+++ FinalProject/eCommerce/src/main/java/miu/edu/ecommerce/repository/OrderRepository.java
@@ -1,23 +1,4 @@
 package miu.edu.ecommerce.repository;
 
-import miu.edu.ecommerce.domain.Order;
-import miu.edu.ecommerce.domain.OrderLine;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
-
-import javax.persistence.Column;
-import java.time.LocalDate;
-import java.util.List;
-import org.springframework.data.repository.query.Param;
-
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-
-    @Query("UPDATE Order o SET o.currentStatus = 'Cancelled' WHERE o.id = :orderId")
-    public Order cancelOrder(long orderId);
-
-    public List<OrderLine> getOrderForBuyer(long orderId);
-
-
-
+public interface OrderRepository {
 }
