5a8432da8eef21447c9409b5c99d847a1750d957
qianzhisanyao <75343488+qianzhisanyao@users.noreply.github.com>
2021-05-12 11:56:54 -0500

    update products for products'detail

--- FinalProject/ecommerce_fe/src/components/Product/Product.js
+++ FinalProject/ecommerce_fe/src/components/Product/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+import Products from '../../containers/Products/Products';
 
 
 export default function Product(props) {
@@ -9,15 +10,17 @@ export default function Product(props) {
       <Link to={`/product/${product.id}`}>
         <img className="medium" src={product.photo} alt={product.productName} />
       </Link>
+      
       <div className="card-body">
         <Link to={`/product/${product.id}`}>
           <h2>{product.name}</h2>
         </Link>
+        {/* <Route path="/product/:id" component={Products} exact></Route> */}
         <div className="row">
           <div className="price">${product.price}</div>
           <div>
-            <Link to={`/seller/${product.producter}`}>
-              {product.producter}
+            <Link to={`/seller/${product.seller}`}>
+              {product.seller}
             </Link>
           </div>
         </div>
--- FinalProject/ecommerce_fe/src/containers/Products/Products.js
+++ FinalProject/ecommerce_fe/src/containers/Products/Products.js
@@ -1,10 +1,136 @@
-import React  from 'react';
-//import Product from '../product/Product';
+import React, { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
+import { APIConfig } from '../../store/API-Config';
+import { Link } from 'react-router-dom';
 
-export default function products() {
-    // <div className="row center">
-    //     {products.map((product) => (
-    //         <Product key={product._id} product={product}></Product>
-    //     ))}
-    // </div>
+
+export default function Products(props) {
+    const APIs = useContext(APIConfig);
+    const productAPI = APIs.productAPI;
+    const productId = props.match.params.id;
+    console.log(productId);
+    const [product, setProduct] = useState([]);
+    //   const product = data.products.find((x) => x.id === props.match.params.id);
+    const [qty, setQty] = useState(1);
+ 
+
+
+  useEffect(() => {
+    const fecthData = async () => {
+        try {
+            const { data } = await axios.get(productAPI);
+            setProduct(data);
+            const productMatch = data.find((x) => x.product.id === props.match.params.id);
+            setProduct(productMatch);
+            console.log(product);
+            console.log("Success data");
+          } catch (err) {
+            console.log(err);
+          }
+        };
+        fecthData();
+      }, []);
+
+  const addToCartHandler = () => {
+    props.history.push(`/shoppingcarts/${productId}?qty=${qty}`);
+  };
+  return (
+
+        <div>
+          <Link to="/">Back to result</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img
+                className="large"
+                src={product.photo}
+                alt={product.productName}
+              ></img>
+            </div>
+            <div className="col-1">
+              <ul>
+                <li>
+                  <h1>{product.productName}</h1>
+                </li>
+                <li>
+
+                    <h2>{product.reviews}</h2>
+  
+                </li>
+                <li>Pirce : ${product.price}</li>
+                <li>
+                  Description:
+                  <p>{product.description}</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col-1">
+              <div className="card card-body">
+                <ul>
+                  <li>
+                    Seller{' '}
+                    {/* <h2>
+                      <Link to={`/seller/${product.seller.id}`}>
+                        {product.seller.seller.name}
+                      </Link>
+                    </h2> */}
+                    {/* <Rating
+                      rating={product.seller.seller.rating}
+                      numReviews={product.seller.seller.numReviews}
+                    ></Rating> */}
+                  </li>
+                  <li>
+                    <div className="row">
+                      <div>Price</div>
+                      <div className="price">${product.price}</div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="row">
+                      <div>Status</div>
+                      <div>
+                        {product.countInStock > 0 ? (
+                          <span className="success">In Stock</span>
+                        ) : (
+                          <span className="danger">Unavailable</span>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                  {product.countInStock > 0 && (
+                    <>
+                      <li>
+                        <div className="row">
+                          <div>Qty</div>
+                          <div>
+                            <select
+                              value={qty}
+                              onChange={(e) => setQty(e.target.value)}
+                            >
+                              {[...Array(product.countInStock).keys()].map(
+                                (x) => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                )
+                              )}
+                            </select>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <button
+                          onClick={addToCartHandler}
+                          className="primary block"
+                        >
+                          Add to Cart
+                        </button>
+                      </li>
+                    </>
+                  )}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+  );
 }
